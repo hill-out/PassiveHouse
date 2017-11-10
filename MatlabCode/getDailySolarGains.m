e234b33382c360c18b110829ef624145d48ef4d4
SamuelRussell <32901430+SamuelRussell@users.noreply.github.com>
2017-11-10 14:18:35 +0000

    New main script

--- MatlabCode/getDailySolarGains.m
+++ MatlabCode/getDailySolarGains.m
@@ -1,8 +1,8 @@
-function [c] = getDailySolarGains()
+%function [c] = getDailySolarGains()
 
 load('weatherSTRUCTtry.mat');
 a = overallSolarGain(wSTRUCTtry.global,wSTRUCTtry.diffuse, [wSTRUCTtry.MONTH,wSTRUCTtry.DAY,wSTRUCTtry.HOUR],[],0.8);
 b = reshape(sum(a,2),24,365);
 c = sum(b);
 
-end
+%end
