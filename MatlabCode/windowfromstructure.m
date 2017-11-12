4167a52f3060864291227686e4d12feda350e18d
SamuelRussell <32901430+SamuelRussell@users.noreply.github.com>
2017-11-12 20:34:43 +0000

    Changed exterior wall surfaces

---
+++ MatlabCode/windowfromstructure.m
@@ -0,0 +1,15 @@
+%function to subtract window area from exterior wall area. 
+
+function [s] = windowfromstructure(structure,windows);
+
+% Windows [x,y,z,L,H,nx,ny,nz,th]
+% Structure [x,y,z,L,H,A,nx,ny,nz,k_insul,L_insul]
+
+for i = 1:1:size(structure,1)
+
+    for k = 1:1:size(windows,1)
+        if structure(i,7:9) == windows(k,5:7)
+            structure(i,6) = structure(i,6) - windows(k,4)*windows(k,5);
+        end
+    end
+end
