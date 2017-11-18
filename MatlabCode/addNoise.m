60cfd65af539deb83a0d8493eafd0969ba4a84a0
Jack <s1310295@sms.ed.ac.uk>
2017-11-18 16:25:02 +0000

    Add files via upload

---
+++ MatlabCode/addNoise.m
@@ -0,0 +1,28 @@
+function [ny] = addNoise(y, step, nP, r)
+% converts temperature data into noisy data
+%
+% y - spme parameter [nx1]
+% step - step 
+% nP - noise Power
+% r - remove 0's
+%
+% ny - noisy y
+
+
+x = 0:1:(numel(y)+1);
+xx = 0:(1/step):((numel(y)+1));
+
+y = [y(1); y; y(end)];
+yy = spline(x,y,xx);
+
+ny = awgn(yy,nP);
+
+if r
+    expandy = reshape(repmat(y,1,2)',[],1);
+    ny(expandy(1:end-1)==0)=0;
+    ny(ny < 0) = 0;
+end
+
+ny = ny(1+floor(step/2):end-step+floor(step/2));
+
+end
