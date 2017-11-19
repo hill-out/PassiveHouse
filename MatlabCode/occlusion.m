8580c1f86644a94b0746fec10aae00fdf91edce6
Jack <s1310295@sms.ed.ac.uk>
2017-11-19 19:17:31 +0000

    minor changes

--- MatlabCode/occlusion.m
+++ MatlabCode/occlusion.m
@@ -8,7 +8,7 @@ function [out, in] = occlusion(sunA, window, f)
 % 
 % occFactor - occlusion factor [1x2] [in, out]
 
-%% calculate stuff blocking the sun from the outside
+%% calculate stuff blocking the sun from the outside (shading)
 
 [az,el,~] = cart2sph(sunA(:,1),sunA(:,2),sunA(:,3)); %calc elevation
 
@@ -23,18 +23,17 @@ barDepth = 0.200; %m
 x = barDepth.*tan(ones(size(barrier))*el);
 
 blockage = 1-barrier.*(x+barWidth)./barSpace;
-out = max([zeros(size(blockage)),min([ones(size(blockage)),blockage]')']')';
+blockage = max([zeros(size(blockage)),min([ones(size(blockage)),blockage]')']')';
 
-out(~barrier) = 1;
+blockage(~barrier) = 1;
 
-%% calculate stuff blocking indoor light from thermal mass
-
-% Wall calculation
-wallDepth = 0.3; %m
+%% window sink calculation
+% relates to the thickness of the wall blocking the sun inside and outside
+wallDepth = 0.46; %m
 
 [azW, elW, ~] = cart2sph(window(:,6),window(:,7),window(:,8));
-dL = abs(wallDepth.*tan(az-azW));
-dH = abs(wallDepth.*tan(el-elW));
+dL = abs(wallDepth/2.*tan(az-azW));
+dH = abs(wallDepth/2.*tan(el-elW));
 
 newL = max([zeros(size(window(:,4))),(window(:,4)-dL)]')';
 newH = max([zeros(size(window(:,5))),(window(:,5)-dH)]')';
@@ -42,6 +41,13 @@ newA = newL.*newH;
 
 A = window(:,4).*window(:,5);
 wallOcc = newA./A;
+wallOcc = max([zeros(size(wallOcc)), min([ones(size(wallOcc)),(wallOcc.*f)]')']')';
+
+%% room size calculation
+% relates to walls stopping the sun hitting the walls
+
+windowBaseHeight = 0.5;
+
 
 in = max([zeros(size(wallOcc)), min([ones(size(wallOcc)),(wallOcc.*f)]')']')';
 
