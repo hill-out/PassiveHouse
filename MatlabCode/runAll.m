f66b24efad91b0bc22a77152089152a685553f97
Jack <s1310295@sms.ed.ac.uk>
2017-11-21 10:19:45 +0000

    Add files via upload

--- MatlabCode/runAll.m
+++ MatlabCode/runAll.m
@@ -1,5 +1,6 @@
 
 load('weatherSTRUCTtry.mat')
 t=[wSTRUCTtry.MONTH,wSTRUCTtry.DAY,wSTRUCTtry.HOUR];
-all2 = overallQT(t(5200,:),t(5300,:),48);
+all2 = overallQT(t(1,:),t(end,:),14*24);
 
+save('all2.mat','all2')
